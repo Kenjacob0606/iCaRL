8c524feaba3f93c66e20d69e14f3ec380013c902
Ken Jacob <kenjacob0606@gmail.com>
2026-02-22 09:54:54 +0000

    test file added

---
+++ test.py
@@ -0,0 +1,14 @@
+import torch
+
+# data = torch.load(
+#     'model/accuracy-22.000_KNN_accuracy-28.000_increment-19_net.pkl', weights_only=False)
+# print(data)
+
+# print("Torch version:", torch.__version__)
+# print("CUDA available:", torch.cuda.is_available())
+# print("CUDA version:", torch.version.cuda)
+# print("Device count:", torch.cuda.device_count())
+
+# print(torch.__file__)
+
+# print(torch.version.cuda)
